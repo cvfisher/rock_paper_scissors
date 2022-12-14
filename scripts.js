c0451733707046a38e54be14784c82120bb1c299
Charlotte <cvfisher.93@gmail.com>
2022-12-14 19:15:23 +0000

    Basic console version of rock paper scissors

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
 const choices = ["rock", "paper", "scissors"];
-const playerSelection = prompt("Make your choice!").toUpperCase();
+let playerSelection = prompt("Make your choice!").toLowerCase();
 let playerScore = 1;
 let computerScore = 1;
-const computerSelection = getComputerChoice();
+let computerSelection = getComputerChoice();
+
 // Randomly returns a choice
 function getComputerChoice() {
   let compSelect = choices[Math.floor(Math.random() * choices.length)];
-  return compSelect.toUpperCase();
+  return compSelect.toLowerCase();
 }
-console.log(getComputerChoice());
+console.log(`Your opponent chose ${computerSelection}`);
 
 // Plays a single round and returns string declaring winner of that round
 function playRound(playerSelection, computerSelection) {
@@ -17,16 +18,40 @@ function playRound(playerSelection, computerSelection) {
     return `You: ${playerScore} Opponent: ${computerScore}`;
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
     console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
-    return `You: ${(playerScore += 1)}`;
+    return `You: ${(playerScore += 1)} Opponent: ${computerScore}`;
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
     console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
-    return `You: ${(playerScore += 1)}`;
+    return `You: ${(playerScore += 1)} Opponent: ${computerScore}`;
   } else if (playerSelection == "paper" && computerSelection == "rock") {
     console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
-    return `You: ${(playerScore += 1)}`;
+    return `You: ${(playerScore += 1)} Opponent: ${computerScore}`;
   } else {
     console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
-    return `Opponent: ${(computerScore += 1)}`;
+    return `You: ${playerScore} Opponent: ${(computerScore += 1)}`;
   }
 }
 console.log(playRound(playerSelection, computerSelection));
+
+//Calls playRound and loops this function 5 times
+function game() {
+  for (i = 0; i < 5; i++) {
+    playerSelection = prompt("Make your choice!").toLowerCase();
+    computerSelection = getComputerChoice();
+    const game = playRound(playerSelection, computerSelection);
+    console.log(game);
+  }
+  if (computerScore > playerScore) {
+    console.log(
+      `Opponent scored ${computerScore} and you scored ${playerScore}. You lose!`
+    );
+  } else if (playerScore === computerScore) {
+    console.log(
+      `Opponent scored ${computerScore} and you scored ${playerScore}. It's a draw!`
+    );
+  } else {
+    console.log(
+      `Opponent scored ${computerScore} and you scored ${playerScore}. Congrats hun. You win!`
+    );
+  }
+}
+game();
